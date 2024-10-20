9b3d2b7dade552137fe8e94e2e318ac8143a1052
Preethi.B <preethi@akkenna.com>
2024-10-20 17:36:41 +0530

    Commit-deploy

--- src/components/career/Banner.tsx
+++ src/components/career/Banner.tsx
@@ -2,39 +2,36 @@ import { Breadcrumb, Col, Container } from "react-bootstrap";
 import "../../styles/banner2.css";
 
 type Props = {
-  title : string,
-  breadcrumb: string,
-}
+  title: string;
+  breadcrumb?: string;
+};
 
-const Banner = ({title,breadcrumb}:Props) => {
 
+const Banner = ({ title, breadcrumb }: Props) => {
   return (
     <div className="breadcrumb-bg pt-5">
-    <Container>
-      <Breadcrumb className="d-flex flex-column">
-        <Col>
-          <h2 className="text-white">{title}</h2>
-        </Col>
-        <Col className="brdcrumb">
-          <Breadcrumb.Item href="/" className="text-white">
-            Home
-          </Breadcrumb.Item>
-          <Breadcrumb.Item active className="text-white">
-            Careers
-          </Breadcrumb.Item>
-          {
-            breadcrumb != "" ? 
-          <Breadcrumb.Item active className="text-white">
-            {breadcrumb}
-          </Breadcrumb.Item>
-          :
-          null
-          }
-        </Col>
-      </Breadcrumb>
-    </Container>
-  </div>
-  )
-}
+      <Container>
+        <Breadcrumb className="d-flex flex-column">
+          <Col>
+            <h2 className="text-white">{title}</h2>
+          </Col>
+          <Col className="brdcrumb">
+            <Breadcrumb.Item href="/" className="text-white">
+              Home
+            </Breadcrumb.Item>
+            <Breadcrumb.Item active className="text-white">
+              Careers
+            </Breadcrumb.Item>
+            {breadcrumb !== "" ? (
+              <Breadcrumb.Item active className="text-white">
+                {breadcrumb}
+              </Breadcrumb.Item>
+            ) : null}
+          </Col>
+        </Breadcrumb>
+      </Container>
+    </div>
+  );
+};
 
-export default Banner
+export default Banner;
